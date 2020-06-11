bdd772b81998a5b4f1b525e6d45775c92bf6b83e
Justin Paoletta <justinpaoletta@gmail.com>
2020-06-10 20:12:40 -0400

    env for challenge_3 setuo

---
+++ challenge_3/client/components/app.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import axios from 'axios';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  componentDidMount(){
+
+  }
+
+  render(){
+    return(
+      <div>Environment Ready! Enjoy Coding!</div>
+    )
+  }
+
+}
+
+export default App;
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,18 @@
+const express = require('express');
+const host = 'localhost';
+const port = 8200;
 
+const server = express();
 
+server.use(express.urlencoded());
+server.use(express.json());
+
+server.listen(port, (error) => {
+  if (error) {
+    console.log('Could not connect to Server', error);
+  } else {
+    console.log(`Project running on http://${host}:${port}`);
+  }
+});
+
+server.use(express.static('./public'));
